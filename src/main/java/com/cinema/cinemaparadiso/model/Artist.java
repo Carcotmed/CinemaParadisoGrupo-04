f208a70b8a169241fd381770419d45bdd38e97b9
Moipanloz <pantion.loza.moises@gmail.com>
2021-03-20 11:41:10 +0100

    ENTIDAD ARTIST
    Creada la entidad Artist y el enum Role
    
    Issue relacionada: #53

---
+++ src/main/java/com/cinema/cinemaparadiso/model/Artist.java
@@ -0,0 +1,27 @@
+package com.cinema.cinemaparadiso.model;
+
+
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name="artists")
+@Getter
+@Setter
+public class Artist extends BaseEntity {
+	
+	@Column(name="roles")
+	private List<Role> roles;
+	
+	@Column(name="projects")
+	@ManyToMany(mappedBy = "team")
+	private List<Project> projects;
+
+}
