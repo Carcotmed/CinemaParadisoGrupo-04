ceb558fe471754f19ea46d6ad304477c22bfde09
Carlos Cote Medina <carcotmed@gmail.com>
2021-03-21 13:35:01 +0100

    AÑADE MÉTODOS DE EDICIÓN Y BUSQUEDA AL SERVICIO
    Se añaden métodos para modificar la contraseña del usuario, listar los usuarios activos y para activar o desactivar un usuario.

--- src/main/java/com/cinema/cinemaparadiso/service/UserService.java
+++ src/main/java/com/cinema/cinemaparadiso/service/UserService.java
@@ -5,6 +5,9 @@ import com.cinema.cinemaparadiso.model.User;
 import com.cinema.cinemaparadiso.repository.AuthoritiesRepository;
 import com.cinema.cinemaparadiso.repository.UserRepository;
 
+import java.util.List;
+import java.util.NoSuchElementException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.stereotype.Service;
@@ -20,6 +23,10 @@ public class UserService {
     public Iterable<User> list(){
         return userRepository.findAll();
     }
+    
+    public List<User> getEnabledUsers() {
+    	return userRepository.findByEnabled(true);
+    }
 
     public void createUser(User user){
         String encryptedPassword = BCrypt.hashpw(user.getPassword(), BCrypt.gensalt(10));
@@ -28,5 +35,29 @@ public class UserService {
         userRepository.save(user);
         Authorities authorities = new Authorities(user.getUsername(),"admin");
         authoritiesRepository.save(authorities);
+    }
+    
+    public void changePassword (String userName, String oldPassword, String newPassword) throws Exception, NoSuchElementException{
+        String encryptedOldPassword = BCrypt.hashpw(oldPassword, BCrypt.gensalt(10));
+        User retrievedUser = userRepository.findById(userName).get();
+        if (retrievedUser.getPassword().equals(encryptedOldPassword)) {
+        	String encryptedNewPassword = BCrypt.hashpw(newPassword, BCrypt.gensalt(10));
+        	retrievedUser.setPassword(encryptedNewPassword);
+        	userRepository.save(retrievedUser);
+        } else {
+        	throw new Exception("The found user's password and the given old passwords dont match");
+        }
+    }
+    
+    public void enableUser(String userName) throws NoSuchElementException {
+    	User retrievedUser = userRepository.findById(userName).get();
+    	retrievedUser.setEnabled(true);
+    	userRepository.save(retrievedUser);
+    }
+    
+    public void disableUser(String userName) throws NoSuchElementException {
+    	User retrievedUser = userRepository.findById(userName).get();
+    	retrievedUser.setEnabled(false);
+    	userRepository.save(retrievedUser);
     }
 }
