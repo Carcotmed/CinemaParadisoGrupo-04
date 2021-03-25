86e1a6556e5ebb141869151babd864f1bd59a6b1
Iván Cárdenas Meneses <ivacarmen@alum.us.es>
2021-03-25 17:59:19 +0100

    INTRODUCE LOS TEST DE SERVICIO
    No habia soporte configurado para test de servicio.
    Aun es necesario implementar los test de controlador.
    
    - Se ha añadido al UserService un metodo count solo para probar los test.
    
    Revisar: #109

---
+++ src/test/java/com/cinema/cinemaparadiso/service/UserServiceTests.java
@@ -0,0 +1,31 @@
+package com.cinema.cinemaparadiso.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import com.cinema.cinemaparadiso.model.User;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class UserServiceTests {
+	
+	@Autowired
+	private UserService userService;	
+
+    @Test
+    public void shouldCountUsers(){
+        long count = userService.countUsers();
+		assertThat(count).isEqualTo(1L);
+    }
+
+	@Test
+	public void shouldCreateUser() {
+        User user = new User();
+		user.setUsername("admin");
+		user.setPassword("admin");
+		user.setEnabled(true);
+        userService.createUser(user);
+		assertThat(userService.countUsers()).isEqualTo(1L);
+	}
+
+}
