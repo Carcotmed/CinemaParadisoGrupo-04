3746e44a56d9696f69190cee1c2c074fb8cf349f
Carlos Cote Medina <carcotmed@gmail.com>
2021-03-28 14:42:16 +0200

    ARREGLA POPULACIÓN DE BD

--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -1,15 +1,5 @@
-CREATE TABLE users(
-	username varchar_ignorecase(255) NOT NULL PRIMARY KEY,
-	password varchar_ignorecase(255) NOT NULL,
-	enabled BOOLEAN NOT NULL
-);
 
+INSERT INTO users(username,password,enabled) VALUES ('adminTest','adminTest',TRUE), ('ivanlot','ivanlot',TRUE), ('user','user',TRUE), ('artist','artist',TRUE);
 
-INSERT INTO users(username,password,enabled) VALUES ('admin','admin',TRUE),
-('adminTest','adminTest',TRUE),
-('ivanlot','ivanlot',TRUE),
-('user','user',TRUE),
-('artist','artist',TRUE);
-
---INSERT INTO users(username,password,enabled) VALUES ('admin','$2a$10$gn.RKrqUiPZuOhBeht0amudVq6eDxe4RB5ARGHa5SLJXig4b7Ollu',TRUE);
---INSERT INTO authorities(username,authority) VALUES ('admin','admin');
+INSERT INTO users(username,password,enabled) VALUES ('admin','$2a$10$gn.RKrqUiPZuOhBeht0amudVq6eDxe4RB5ARGHa5SLJXig4b7Ollu',TRUE);
+INSERT INTO authorities(username,authority) VALUES ('admin','admin');
