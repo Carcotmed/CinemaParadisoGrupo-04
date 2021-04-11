fd6da50dc2a79b0b5ed8c046dc84f6cf68ef1dc0
Capi <sergiopeca98@gmail.com>
2021-04-11 09:23:55 +0200

    AÑADIDO MENSAJE AL QUERER UNIRTE A UN PROYECTO

--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -5,7 +5,7 @@ INSERT INTO users(username,password,enabled,email) VALUES ('admin3','$2a$10$2B0f
 INSERT INTO users(username,password,enabled,email) VALUES ('admin4','$2a$10$lAuC/wtbSo22eMCtSgDgxe/N3q2QnNjYxIhvTKzudFhtH1pICCzZe',TRUE,'user5@ejemplo.com');
 INSERT INTO users(username,password,enabled,email) VALUES ('admin5','$2a$10$lAuC/wtbSo22eMCtSgDgxe/N3q2QnNjYxIhvTKzudFhtH1p3CCzZe',TRUE,'user6@ejemplo.com');
 INSERT INTO users(username,password,enabled,email) VALUES ('admin6','$2a$10$lAuC/wtbSo22eMCtSgDgxe/N3q2QnNjYxIhvTKzudFhtH1p2CCzZe',TRUE,'user7@ejemplo.com');
-INSERT INTO users(username,password,enabled,email) VALUES ('admin7','$2a$10$lAuC/wtbSo22eMCtSgDgxe/N3q2QnNjYxIhvTKzudFhtH1p1CCzZe',TRUE,'user8@ejemplo.com');
+INSERT INTO users(username,password,enabled,email) VALUES ('admin7','$2a$10$gn.RKrqUiPZuOhBeht0amudVq6eDxe4RB5ARGHa5SLJXig4b7Ollu',TRUE,'user8@ejemplo.com');
 
 
 INSERT INTO authorities(username,authority) VALUES ('admin0','admin');
@@ -53,19 +53,19 @@ INSERT INTO messages(id,version,issue,body,messageDate,emisor_id,receptor_id) VA
 
 
 
-INSERT INTO projects (id,description,title,photo,pro,genre,version) VALUES (1,'De miedo','Miedo','https://i.pinimg.com/originals/73/09/3f/73093ff700637e9ce2aed290b9be255c.jpg',TRUE,2,0);
-INSERT INTO projects (id,description,title,photo,pro,genre,version) VALUES (2,'Ciencia ficcion','Ciencia Ficcion','https://miro.medium.com/max/2400/1*onNN4P1jCC8DyS9GxUAm1w.jpeg',TRUE,1,0);
-INSERT INTO projects (id,description,title,photo,pro,genre,version) VALUES (3,'De accion','Accion','https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/john-wick-chapter-3-parabellum-1558983194.jpg',TRUE,0,0);
-INSERT INTO projects (id,description,title,photo,pro,genre,version) VALUES (4,'De Psicologica','Psicologica','https://media.gq.com.mx/photos/5e4ec0e59e37340009e81f24/16:9/w_1920,c_limit/peliculas-psicologicas-en-netflix.jpg',TRUE,3,0);
-INSERT INTO projects (id,description,title,photo,pro,genre,version) VALUES (5,'De humor','Humoristicas','https://elcomercio.pe/resizer/6My5hxvD2FdlVZNPT1TrNZfrUEg=/1200x900/smart/filters:format(jpeg):quality(75)/arc-anglerfish-arc2-prod-elcomercio.s3.amazonaws.com/public/SA2H5DWGFBFXBDVY5BRANX6IYA.jpg',TRUE,4,0);
-INSERT INTO projects (id,description,title,photo,pro,genre,version) VALUES (6,'De ficcion','Ficcion','https://i1.wp.com/cinefilosoficial.com/wp-content/uploads/2020/07/harry-potter-ficcion.jpg',TRUE,5,0);
-INSERT INTO projects (id,description,title,photo,pro,genre,version) VALUES (7,'Projecto de terror','Scary','https://www.elcomercio.com/files/article_main/uploads/2019/09/05/5d71926073d7d.jpeg',FALSE,2,0);
-INSERT INTO projects (id,description,title,photo,pro,genre,version) VALUES (8,'De galaxias','Star wars','https://i.blogs.es/37bccb/1366_2000/450_1000.jpeg',FALSE,1,0);
-INSERT INTO projects (id,description,title,photo,pro,genre,version) VALUES (9,'De tiroteos','Juan bond','https://e00-elmundo.uecdn.es/assets/multimedia/imagenes/2019/12/10/15760116146075.jpg',FALSE,0,0);
-INSERT INTO projects (id,description,title,photo,pro,genre,version) VALUES (10,'Una vidente te lee la mente','Veo tu futuro','https://www.ecestaticos.com/image/clipping/868e66d9da6a671cab0b69f31baf93b3/los-trucos-que-utilizan-los-videntes-para-enganarte-contados-por-una-de-ellos.jpg',FALSE,3,0);
-INSERT INTO projects (id,description,title,photo,pro,genre,version) VALUES (11,'Para partirte de risa','Mortadelo y Filemon Indie','https://estaticos.muyhistoria.es/media/cache/1140x_thumb/uploads/images/ephemeris/5e04bfc55cafe8c5f6f862fe/mortadelo-filemon_0.jpg',FALSE,4,0);
-INSERT INTO projects (id,description,title,photo,pro,genre,version) VALUES (12,'Ficcion total','Futurismo','https://estaticos.muyinteresante.es/media/cache/760x570_thumb/uploads/images/gallery/5c0fc5e35cafe803d35b883b/peliculas-2019_0.jpg',FALSE,5,0);
-INSERT INTO projects (id,description,title,photo,pro,genre,version) VALUES (13,'Locuras Extremas','Futurismo','http://pxsports.com/wp-content/uploads/2016/12/francia-1.jpg',FALSE,5,0);
+INSERT INTO projects (id,description,title,photo,pro,genre,version,my_admin) VALUES (1,'De miedo','Miedo','https://i.pinimg.com/originals/73/09/3f/73093ff700637e9ce2aed290b9be255c.jpg',TRUE,2,0,'admin0');
+INSERT INTO projects (id,description,title,photo,pro,genre,version,my_admin) VALUES (2,'Ciencia ficcion','Ciencia Ficcion','https://miro.medium.com/max/2400/1*onNN4P1jCC8DyS9GxUAm1w.jpeg',TRUE,1,0,'admin0');
+INSERT INTO projects (id,description,title,photo,pro,genre,version,my_admin) VALUES (3,'De accion','Accion','https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/john-wick-chapter-3-parabellum-1558983194.jpg',TRUE,0,0,'admin0');
+INSERT INTO projects (id,description,title,photo,pro,genre,version,my_admin) VALUES (4,'De Psicologica','Psicologica','https://media.gq.com.mx/photos/5e4ec0e59e37340009e81f24/16:9/w_1920,c_limit/peliculas-psicologicas-en-netflix.jpg',TRUE,3,0,'admin0');
+INSERT INTO projects (id,description,title,photo,pro,genre,version,my_admin) VALUES (5,'De humor','Humoristicas','https://elcomercio.pe/resizer/6My5hxvD2FdlVZNPT1TrNZfrUEg=/1200x900/smart/filters:format(jpeg):quality(75)/arc-anglerfish-arc2-prod-elcomercio.s3.amazonaws.com/public/SA2H5DWGFBFXBDVY5BRANX6IYA.jpg',TRUE,4,0,'admin0');
+INSERT INTO projects (id,description,title,photo,pro,genre,version,my_admin) VALUES (6,'De ficcion','Ficcion','https://i1.wp.com/cinefilosoficial.com/wp-content/uploads/2020/07/harry-potter-ficcion.jpg',TRUE,5,0,'admin0');
+INSERT INTO projects (id,description,title,photo,pro,genre,version,my_admin) VALUES (7,'Projecto de terror','Scary','https://www.elcomercio.com/files/article_main/uploads/2019/09/05/5d71926073d7d.jpeg',FALSE,2,0,'admin0');
+INSERT INTO projects (id,description,title,photo,pro,genre,version,my_admin) VALUES (8,'De galaxias','Star wars','https://i.blogs.es/37bccb/1366_2000/450_1000.jpeg',FALSE,1,0,'admin0');
+INSERT INTO projects (id,description,title,photo,pro,genre,version,my_admin) VALUES (9,'De tiroteos','Juan bond','https://e00-elmundo.uecdn.es/assets/multimedia/imagenes/2019/12/10/15760116146075.jpg',FALSE,0,0,'admin0');
+INSERT INTO projects (id,description,title,photo,pro,genre,version,my_admin) VALUES (10,'Una vidente te lee la mente','Veo tu futuro','https://www.ecestaticos.com/image/clipping/868e66d9da6a671cab0b69f31baf93b3/los-trucos-que-utilizan-los-videntes-para-enganarte-contados-por-una-de-ellos.jpg',FALSE,3,0,'admin0');
+INSERT INTO projects (id,description,title,photo,pro,genre,version,my_admin) VALUES (11,'Para partirte de risa','Mortadelo y Filemon Indie','https://estaticos.muyhistoria.es/media/cache/1140x_thumb/uploads/images/ephemeris/5e04bfc55cafe8c5f6f862fe/mortadelo-filemon_0.jpg',FALSE,4,0,'admin0');
+INSERT INTO projects (id,description,title,photo,pro,genre,version,my_admin) VALUES (12,'Ficcion total','Futurismo','https://estaticos.muyinteresante.es/media/cache/760x570_thumb/uploads/images/gallery/5c0fc5e35cafe803d35b883b/peliculas-2019_0.jpg',FALSE,5,0,'admin0');
+INSERT INTO projects (id,description,title,photo,pro,genre,version,my_admin) VALUES (13,'Locuras Extremas','Futurismo','http://pxsports.com/wp-content/uploads/2016/12/francia-1.jpg',FALSE,5,0,'admin0');
 
 
 INSERT INTO artists (id,description,name,sur_name,pro,summary,username,role,photo,version,left_projects) VALUES (1,'pepitod','pepito1','pepito1sur',TRUE,'pepito1sum','admin0','CAMARA','https://i.blogs.es/5efe2c/cap_001/450_1000.jpg',0,3);
