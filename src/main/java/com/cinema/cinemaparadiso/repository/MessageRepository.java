667806336f01416fd541a38ade100a970714690f
Oscar <oscdoraba@alum.us.es>
2021-04-16 19:00:29 +0200

    GENERA LIMITE DE PETICIÓN PARA ENTRAR A UN PROYECTO ARTISTA/PRODUCER

--- src/main/java/com/cinema/cinemaparadiso/repository/MessageRepository.java
+++ src/main/java/com/cinema/cinemaparadiso/repository/MessageRepository.java
@@ -1,11 +1,13 @@
 package com.cinema.cinemaparadiso.repository;
 
-import com.cinema.cinemaparadiso.model.Message;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import com.cinema.cinemaparadiso.model.Message;
 
 @Repository
 public interface MessageRepository extends CrudRepository<Message,Integer>{
@@ -15,5 +17,8 @@ public interface MessageRepository extends CrudRepository<Message,Integer>{
 
 	@Query("SELECT ALL m FROM Message m WHERE m.receptor.username = :usernameQuery")
 	public Iterable<Message> findByReceptorUsername(@Param("usernameQuery") String username);
+
+	@Query("SELECT m From Message m WHERE (m.body = ?1 AND m.receptor.username = ?2 AND m.emisor.username = ?3)")
+	public Optional<Message> requestExist(@Param("body") String body,@Param("receptor") String receptor,@Param("emisor") String emisor);
 	
 }
