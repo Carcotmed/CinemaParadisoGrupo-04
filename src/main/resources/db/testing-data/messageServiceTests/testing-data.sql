9f36d5830a05d8600eb465d8a9ccbd41aac51503
Carlos Cote Medina <carcotmed@alum.us.es>
2021-04-29 13:26:07 +0200

    AÑADE TESTS DE SERVICIO DE MESSAGE

---
+++ src/main/resources/db/testing-data/messageServiceTests/testing-data.sql
@@ -0,0 +1,5 @@
+INSERT INTO users(username,password,enabled,email) VALUES ('autoCreatedTestUser1','does not matter',TRUE,'autoCreatedUser1@cinemaparadiso.com');
+INSERT INTO users(username,password,enabled,email) VALUES ('autoCreatedTestUser2','does not matter',TRUE,'autoCreatedUser2@cinemaparadiso.com');
+
+INSERT INTO messages (id,version,issue, body, messagedate, emisor_id, receptor_id,is_request) VALUES (1,0,'Test Issue 1','Test Body 1',TO_DATE('2021/04/10', 'YYYY/MM/DD'),'autoCreatedTestUser1','autoCreatedTestUser2',false);
+INSERT INTO messages (id,version,issue, body, messagedate, emisor_id, receptor_id,is_request) VALUES (2,0,'Test Issue 2','Test Body 2',TO_DATE('2021/04/10', 'YYYY/MM/DD'),'autoCreatedTestUser1','autoCreatedTestUser2',false);
--- src/test/java/com/cinema/cinemaparadiso/service/MessageServiceTests.java
+++ src/test/java/com/cinema/cinemaparadiso/service/MessageServiceTests.java
@@ -1,57 +1,182 @@
 package com.cinema.cinemaparadiso.service;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.cinema.cinemaparadiso.model.Artist;
+import com.cinema.cinemaparadiso.model.Message;
+import com.cinema.cinemaparadiso.model.Producer;
+import com.cinema.cinemaparadiso.model.Project;
+import com.cinema.cinemaparadiso.model.Rel_projects_story;
+import com.cinema.cinemaparadiso.model.Story;
+import com.cinema.cinemaparadiso.model.User;
+import com.cinema.cinemaparadiso.model.Writer;
+
 @SpringBootTest
-@Sql("/db/test.sql")
+@Sql("/db/testing-data/messageServiceTests/testing-data.sql")
 @Transactional
 public class MessageServiceTests {
-	
 
-/*
-    @Test
-	public void shouldFindMessageById() {
-    	Message message = messageService.findByUsername("userTest1").iterator().next();
-    	Message sameMessage = messageService.findById(message.getId());
-    	
-    	assertEquals(message, sameMessage);
+	@Autowired
+	public MessageService messageService;
+
+	@Autowired
+	public UserService userService;
+
+	@Test
+	public void findByIdTest() {
+
+		Integer messageId = 1;
+		Message retrievedMessage = messageService.findById(messageId);
+		assertEquals("Test Body 1", retrievedMessage.getBody());
+		assertEquals("autoCreatedTestUser1", retrievedMessage.getEmisor().getUsername());
+		assertEquals("autoCreatedTestUser2", retrievedMessage.getReceptor().getUsername());
+		assertEquals("Test Issue 1", retrievedMessage.getIssue());
+
 	}
-    
-    @Test
-    public void shouldFindMessagesByUsername() {
-    	Message message = messageService.findByUsername("userTest1").iterator().next();
-    	assertEquals(message.getBody(), "Test Body 1");
-    }
-    
-    @Test
-    public void shouldCreateMessage() {
-    	Message message = new Message();
-    	message.setIssue("New Test Issue");
-    	message.setBody("New Test Body");
-    	message.setEmisor(userService.getUserByUsername("userTest1"));
-    	message.setReceptor(userService.getUserByUsername("userTest1"));
-    	
-    	messageService.create(message);
-    	Message savedMessage = messageService.findById(message.getId());
-    	assertNotNull(savedMessage);
-    	assertEquals(savedMessage.getBody(), "New Test Body");
-    	assertEquals(savedMessage.getIssue(), "New Test Issue");
 
-    }
-    
-    @Test
-    public void shouldDeleteMessage() {
-    	Message message = messageService.findByUsername("userTest1").iterator().next();
-    	Integer messageId = message.getId();
-    	
-    	messageService.delete(message);
-    	
-    	assertThrows(NoSuchElementException.class, () -> messageService.findById(messageId));
-    }
-    
-    */
+	@Test
+	public void findByEmisorUsernameTest() {
+
+		String username = "autoCreatedTestUser1";
+		List<Message> retrievedMessages = new ArrayList<>();
+		messageService.findByEmisorUsername(username).forEach(x -> retrievedMessages.add(x));
+
+		assertEquals(2, retrievedMessages.size());
+		Message message1 = retrievedMessages.get(0);
+		Message message2 = retrievedMessages.get(1);
+
+		assertEquals("Test Body 1", message1.getBody());
+		assertEquals("autoCreatedTestUser1", message1.getEmisor().getUsername());
+		assertEquals("autoCreatedTestUser2", message1.getReceptor().getUsername());
+		assertEquals("Test Issue 1", message1.getIssue());
+
+		assertEquals("Test Body 2", message2.getBody());
+		assertEquals("autoCreatedTestUser1", message2.getEmisor().getUsername());
+		assertEquals("autoCreatedTestUser2", message2.getReceptor().getUsername());
+		assertEquals("Test Issue 2", message2.getIssue());
+
+	}
+
+	@Test
+	public void findByReceptorUsernameTest() {
+
+		String username = "autoCreatedTestUser2";
+		List<Message> retrievedMessages = new ArrayList<>();
+		messageService.findByReceptorUsername(username).forEach(x -> retrievedMessages.add(x));
+
+		assertEquals(2, retrievedMessages.size());
+		Message message1 = retrievedMessages.get(0);
+		Message message2 = retrievedMessages.get(1);
+
+		assertEquals("Test Body 1", message1.getBody());
+		assertEquals("autoCreatedTestUser1", message1.getEmisor().getUsername());
+		assertEquals("autoCreatedTestUser2", message1.getReceptor().getUsername());
+		assertEquals("Test Issue 1", message1.getIssue());
+
+		assertEquals("Test Body 2", message2.getBody());
+		assertEquals("autoCreatedTestUser1", message2.getEmisor().getUsername());
+		assertEquals("autoCreatedTestUser2", message2.getReceptor().getUsername());
+		assertEquals("Test Issue 2", message2.getIssue());
+
+	}
+
+	@Test
+	public void createTest(){
+		User user1 = userService.getUserByUsername("autoCreatedTestUser1");
+		User user2 = userService.getUserByUsername("autoCreatedTestUser2");
+
+		Message message = new Message("Created Test Issue", "Created Test Body", Date.from(Instant.now()), user2, user1);
+		message.setId(99);
+		
+		messageService.create(message);
+		
+		Message retrievedMessage = messageService.findById(99);
+		
+		
+	}
+
+//	@Test
+//	public void delete(Message message) throws IllegalArgumentException {
+//	}
+//
+//	@Test
+//	public void requestAlreadyExistArtist(Integer projectId, Integer artistId) {
+//	}
+//
+//	@Test
+//	public void requestToEnterProjectArtist(Integer projectId, Integer artistId) {
+//	}
+//
+//	@Test
+//	public void requestAlreadyExistProducer(Integer projectId, Integer producerId) {
+//	}
+//
+//	@Test
+//	public void requestToEnterProjectStory(Integer projectId, Integer storyId) {
+//	}
+//
+//	@Test
+//	public void requestToEnterProjectProducer(Integer projectId, Integer producerId) {
+//	}
+//
+//	@Test
+//	public void acceptRequestStory(Integer messageId) {
+//	}
+//
+//	@Test
+//	public void acceptRequestArtist(Integer messageId) {
+//	}
+//
+//	@Test
+//	public void acceptRequestProducer(Integer messageId) {
+//	}
+//
+//	@Test
+//	public void rejectRequestArtist(Integer messageId) {
+//	}
+//
+//	@Test
+//	public void rejectRequestStory(Integer messageId) {
+//	}
+//
+//	@Test
+//	public void rejectRequestProducer(Integer messageId) {
+//	}
+//
+//	@Test
+//	public void findAllReceivedMessages(String username) {
+//	}
+//
+//	@Test
+//	public void findAllSentsMessages(String username) {
+//	}
+//
+//	@Test
+//	public void deleteAllByStoryId(Integer storyId) {
+//	}
+//
+//	@Test
+//	public void checkMessage(String username) {
+//	}
+//
+//	@Test
+//	public void checkSeen(Integer messageId) {
+//	}
 
 }
